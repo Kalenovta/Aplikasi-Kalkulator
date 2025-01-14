69fd3dbbedcf974a9f2c2102c9ae42de72a8740c
Kalenovta <Kalenovtakhoirulanwar@gmail.com>
2025-01-14 07:17:17 +0700

    Add files via upload

---
+++ UKK_KALKULATOR/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalkulator Online</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+    <form action="hasil.php">
+         <h1>Kalkulator Online</h1>
+         <input type="number" name= "angka1">
+         <select name="operator">
+              <option>+</option>
+              <option>-</option>
+              <option>:</option>
+              <option>x</option>
+         <input type="number" name= "angka2">
+         <button type="sumbit">Hitung</button>
+         </select>
+    </form>
+    
+</body>
+</html>
